c7b6376634a61f5e2a27c0082b577019f9f51587
Samuel Martin <samumartinfrias@gmail.com>
2025-08-21 19:14:15 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import DarkModeSelector from '$lib/components/ui/DarkModeSelector.svelte';
+	
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
+<DarkModeSelector />
+{@render children?.()}
